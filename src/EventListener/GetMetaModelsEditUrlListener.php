9a28568f30b0ca8144c9e131f5614f74b0900994
fritzmg <fmg@inspiredminds.at>
2026-08-05 18:08:00 +0100

    Better MetaModel support

--- src/EventListener/GetMetaModelsEditUrlListener.php
+++ src/EventListener/GetMetaModelsEditUrlListener.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 namespace InspiredMinds\ContaoSearchAndReplace\EventListener;
 
 use ContaoCommunityAlliance\DcGeneral\Data\ModelId;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Column;
 use InspiredMinds\ContaoSearchAndReplace\Event\GetEditUrlEvent;
 use MetaModels\IFactory;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
@@ -18,6 +20,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 class GetMetaModelsEditUrlListener
 {
     public function __construct(
+        private readonly Connection $db,
         private readonly UrlGeneratorInterface $urlGenerator,
         private readonly IFactory $metaModelsFactory,
     ) {
@@ -25,17 +28,48 @@ class GetMetaModelsEditUrlListener
 
     public function __invoke(GetEditUrlEvent $event): void
     {
-        if (!$this->metaModelsFactory?->getMetaModel($event->table)) {
-            return;
+        $table = $event->table;
+        $id = $event->id;
+
+        // Check if this is a MetaModel record directly
+        if (str_starts_with($table, 'mm_') && $this->db->fetchOne('SELECT TRUE FROM tl_metamodel WHERE tableName = ?', [$table])) {
+            $mmTable = $table;
+            $mmItemId = $id;
+        } else {
+            // Check if this is a MetaModel attribute
+            if (!str_starts_with($table, 'tl_metamodel_')) {
+                return;
+            }
+
+            $schemaManager = $this->db->createSchemaManager();
+            $columns = $schemaManager->listTableColumns($table);
+
+            if (3 !== \count(array_filter($columns, static fn (Column $column) => \in_array($column->getName(), ['id', 'att_id', 'item_id'], true)))) {
+                return;
+            }
+
+            // Fetch the MetaModel attribute record
+            if (!$attrRecord = $this->db->fetchAssociative(\sprintf('SELECT att_id, item_id FROM %s WHERE id = ?', $this->db->quoteIdentifier($table)), [$id])) {
+                return;
+            }
+
+            $mmItemId = $attrRecord['item_id'];
+            $attId = $attrRecord['att_id'];
+
+            if (!$mmId = $this->db->fetchOne('SELECT pid FROM tl_metamodel_attribute WHERE id = ?', [$attId])) {
+                return;
+            }
+
+            $mmTable = $this->metaModelsFactory->translateIdToMetaModelName($mmId);
         }
 
         $event->setEditUrl(
             $this->urlGenerator->generate(
                 'metamodels.metamodel',
                 [
-                    'tableName' => $event->table,
+                    'tableName' => $mmTable,
                     'act' => 'edit',
-                    'id' => ModelId::fromValues($event->table, $event->id)->getSerialized(),
+                    'id' => ModelId::fromValues($mmTable, $mmItemId)->getSerialized(),
                 ],
             ),
         );
